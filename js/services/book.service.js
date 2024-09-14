47638d2f69e29256ce29b2da15603005d4453887
Valery Vishnevezky <valery020201@gmail.com>
2024-09-14 19:43:04 +0300

    Add check for books in storage and loading books to storage and update function to save changes to storage and function addUserBook + getBookById

--- js/services/book.service.js
+++ js/services/book.service.js
@@ -1,56 +1,86 @@
+'use strict'
 
-// const BOOKS_KEY = 'books'
-// _getBooks()
+const BOOKS_KEY = 'books'
 
-const gBooks = [
-    {
-        id: makeid(6),
-        title: 'Harry Potter',
-        price: 260,
-        imgUrl: 'lori-ipsi.jpg'
-    },
-    {
-        id: makeid(6),
-        title: 'The Little Prince',
-        price: 150,
-        imgUrl: 'lori-ipsi.jpg'
-    },
-    {
-        id: makeid(6),
-        title: 'The Da Vinci Code',
-        price: 170,
-        imgUrl: 'lori-ipsi.jpg'
-    },
-    {
-        id: makeid(6),
-        title: 'Dune',
-        price: 350,
-        imgUrl: 'lori-ipsi.jpg'
-    },
-    {
-        id: makeid(6),
-        title: 'The Godfather',
-        price: 200,
-        imgUrl: 'lori-ipsi.jpg'
+function initBooks() {
+    var books = loadFromStorage(BOOKS_KEY)
+
+    if (!books) {
+        books = [
+            {
+                id: makeid(6),
+                title: 'Harry Potter',
+                price: 260,
+                imgUrl: 'lori-ipsi.jpg'
+            },
+            {
+                id: makeid(6),
+                title: 'The Little Prince',
+                price: 150,
+                imgUrl: 'lori-ipsi.jpg'
+            },
+            {
+                id: makeid(6),
+                title: 'The Da Vinci Code',
+                price: 170,
+                imgUrl: 'lori-ipsi.jpg'
+            },
+            {
+                id: makeid(6),
+                title: 'Dune',
+                price: 350,
+                imgUrl: 'lori-ipsi.jpg'
+            },
+            {
+                id: makeid(6),
+                title: 'The Godfather',
+                price: 200,
+                imgUrl: 'lori-ipsi.jpg'
+            }
+        ]
+        saveToStorage(BOOKS_KEY, books)
     }
-]
+    return books
+}
+
+const gBooks = initBooks()
 console.log(gBooks)
 
 function getBooks() {
-return gBooks
+    return gBooks
 }
 
 
-function removeBook(bookId){
+function removeBook(bookId) {
     const idx = gBooks.findIndex(book => book.id === bookId)
     gBooks.splice(idx, 1)
-    renderBooks()
+    _saveBooks()
 }
 
-function updatePrice(newPrice, bookId){
-    gBooks.forEach(book => {
+function updatePrice(price, bookId) {
+    gBooks.find(book => {
         if (book.id === bookId) {
-            return book.price = newPrice
+            return book.price = price
         }
     })
+    _saveBooks()
+}
+
+function addUserBook(title, price) {
+    const userBook = {
+        id: makeid(6),
+        title: title,
+        price: price,
+        imgUrl: 'lori-ipsi.jpg'
+    }
+    gBooks.push(userBook)
+    _saveBooks()
+}
+
+function getBookById(bookId) {
+    return gBooks.find(book => book.id === bookId)
+}
+
+function _saveBooks() {
+    saveToStorage(BOOKS_KEY, gBooks)
 }
