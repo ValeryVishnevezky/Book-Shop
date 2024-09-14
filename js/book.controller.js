d818949e55b3f28c921256abd85224c39679482a
Valery Vishnevezky <valery020201@gmail.com>
2024-09-14 19:39:06 +0300

    Change onRemoveBook add  onAddBook onAddBook and onShowBookDetails

--- js/book.controller.js
+++ js/book.controller.js
@@ -1,5 +1,7 @@
+'use strict'
 
 function onInit() {
+    initBooks()
     renderBooks()
 }
 
@@ -22,20 +24,41 @@ function renderBooks() {
 
     books.forEach(book => {
         rowsTbody += `<tr class="book">
-    <td class="book-title">${book.title}</td>
+    <td class="book-title">${book.title}<button class="book-details-button" onClick = "onShowBookDetails('${book.id}')">i</button></td>
     <td class="book-price">${book.price}</td>
     <td class="book-actions">
-      <button class="read">Read</button>
-      <button class="update" onClick = "onUpdateBook('${book.id}')">Update</button>
-      <button class= "delete" onClick = "removeBook('${book.id}')">Delete</button>
+      <button class="button-read">Read</button>
+      <button class="button-update" onClick = "onUpdateBook('${book.id}')">Update</button>
+      <button class= "button-delete" onClick = "onRemoveBook('${book.id}')">Delete</button>
     </td>
   </tr>`
     })
     elTbody.innerHTML = rowsTbody
 }
 
-function onUpdateBook(bookId){
+function onRemoveBook(bookId) {
+    removeBook(bookId)
+    renderBooks()
+}
+
+function onUpdateBook(bookId) {
     const userPrice = prompt('Enter your price:')
     updatePrice(userPrice, bookId)
     renderBooks()
+}
+
+function onAddBook() {
+    const userBookName = prompt('Enter your book title:')
+    const userBookPrice = prompt('Enter your book price:')
+    addUserBook(userBookName, userBookPrice)
+    renderBooks()
+}
+
+function onShowBookDetails(bookId){
+    const elModal = document.querySelector('.book-details')
+    const elDetails = elModal.querySelector('pre')
+
+    const book = getBookById(bookId)
+    elDetails.innerHTML = `<h1>${book.title}</h1><p>Price: ${book.price}</p>`
+    elModal.showModal()
 }
